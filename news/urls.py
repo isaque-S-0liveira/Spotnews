15ee8835b909c11db1a1315bbb0815a27b7f7a1a
Isaque <zazac3179@gmail.com>
2023-09-25 14:56:12 -0300

    implementação do template news_details.html que mostra os detalhes de uma notícia

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 # events/urls.py
 from django.urls import path
-from news.views import index
+from news.views import index, new_detail
 
 
 urlpatterns = [
     path("", index, name="home-page"),
+    path("news/<int:new_id>", new_detail, name="news-details-page"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from news.models.news_model import News
 
 
 def index(request):
     context = {"news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def new_detail(request, new_id):
+    try:
+        context = get_object_or_404(News, id=new_id)
+        return render(
+            request,
+            "news_details.html",
+            {"new": context, "categories": context.categories.all()},
+        )
+    except Http404:
+        return render(request, "404.html")
